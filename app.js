455366e6279c19c7156a4c86ed93d0375af20902
Daniel <u1201416@unimilitar.edu.co>
2019-09-26 15:50:31 -0500

    primer intento

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const mongo = require('mongoose');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+
+const app = express();
+const saltRounds = 10;
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cookieSession({
+    secret: 'holaasdljaslkjdsalksadlkjsadlk',  
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+
+var schema = mongo.Schema({
+    email: String,
+    password: String,
+    name: String    
+});
+
+var User = mongo.model('User', schema);
+mongo.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-autenticar', {useNewUrlParser:true,  useUnifiedTopology: true });
+mongo.connection.on('error', (e)=>{console.error(e)});
+
+const requireUser = async (req, res, next) => {
+    const userId = req.session.userId;
+    if (userId) {
+      const user = await User.findOne({ _id: userId });
+      res.locals.user = user;
+      next();
+    } else {
+      return res.redirect("/login");
+    }
+  }
+
+app.get('/login', (req, res)=>{
+    if(req.session.userId){
+        return res.redirect('/');
+    }
+    res.render('form', {error: null});        
+});
+
+app.get('/logout', (req, res)=>{
+    req.session.userId = null;
+    return res.redirect('/login');
+});
+app.get('/', requireUser, async (req, res)=>{
+    const users = await User.find(); 
+    res.render("users", { users: users }); 
+});
+
+app.get('/register', (req,res)=>{
+    if(req.session.userId){
+        return res.redirect('/');
+    }
+    res.render('form-register');
+});
+app.post('/register',async (req,res)=>{
+    
+    const currentUser = await User.findOne({ email: req.body.email });
+    if(currentUser){
+        res.render('form', {message: 'user exists, login?'});
+    }
+    else{
+        var salt = bcrypt.genSaltSync(saltRounds);
+        var hash = bcrypt.hashSync(req.body.password, salt);
+        await User.create({email:req.body.email, password:hash, name: req.body.name});
+        const createdUser = await User.findOne({email:req.body.email})
+        req.session.userId = createdUser._id;
+        return res.redirect('/');
+    }
+})
+app.post('/login', async (req, res)=>{
+    const currentUser = await User.findOne({ email: req.body.email });
+    if(currentUser){
+        try {
+            bcrypt.compare(req.body.password, currentUser.password, function(err, exists) {
+                if(exists) {
+                    req.session.userId = currentUser._id;
+                    return res.redirect('/');
+                } else {
+                    res.render('form', {message:'wrong password'})
+                } 
+              });           
+        } catch (error) {        
+            console.log(error);
+        }        
+    }else{
+        res.render('form-register', {message: 'user not found, register?'})
+    }    
+})
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+/*
+mostrar todos los usuarios, parece que está sobreescribiendo la cookie de sesión
+return req.redirect? 
+um next()?
+buscar usiario despues de crearlo y/n?
+loca(l)s?
+bcrypt safe?
+*/
